cc6ac347fb1a8fb39633867e93013abf85241dab
Juan <juanalegre@gmail.com>
2014-06-16 20:34:16 +0200

    New version

--- server.R
+++ server.R
@@ -5,19 +5,19 @@ mpgData <- mtcars
 shinyServer(function(input, output) {
   Text <- reactive({
     x<-length(input$checkbox)
-    if (x==9){"No predictors selected"}
-    else if(x==10){paste(input$checkbox[1])}
-    else if(x==11){paste(input$checkbox[1],"+",input$checkbox[2])}
-    else if(x==12){paste(input$checkbox[1],"+",input$checkbox[2],"+",input$checkbox[3])}
+    if (x==9){"No predictors selected"} #for local version should be x==0
+    else if(x==10){paste(input$checkbox[1])} #for local version should be x==1
+    else if(x==11){paste(input$checkbox[1],"+",input$checkbox[2])} #for local version should be x==2
+    else if(x==12){paste(input$checkbox[1],"+",input$checkbox[2],"+",input$checkbox[3])} #for local version should be x==3
   })
   modelText <- reactive(paste("mpg ~", Text()))
   output$predictors <-renderText({Text()})
   output$model <- renderText({
-    if (length(input$checkbox)==9){"No predictors selected"}
+    if (length(input$checkbox)==9){"No predictors selected"} #for local version should be x==0
     else{modelText()}
   })
   output$prediction <- renderText({
-    if (length(input$checkbox)==9){"No predictors selected"}
+    if (length(input$checkbox)==9){"No predictors selected"} #for local version should be x==0
     else{
       fit<-lm(as.formula(modelText()),data=mtcars)
       c<-input$cr
